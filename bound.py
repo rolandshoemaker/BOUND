6d6dc64208c8eca1561eb4484cd0b232df26fc55
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-11-04 18:55:38 -0800

    lots of stuff, starting to actually work now

--- bound.py
+++ bound.py
@@ -3,8 +3,8 @@ import config
 from flask import Flask, render_template, request, jsonify
 import sys
 sys.path.insert(1, '/usr/local/lib/python3.4/dist-packages/iscpy/') # guh such ghetto-hack to make this work with python 3...
-import pickle, iscpy
-import dns.zone
+import pickle, iscpy, difflib
+import dns.zone, dns.rdatatype
 from redis import StrictRedis
 
 app = Flask(__name__)
@@ -25,12 +25,14 @@ def get_local_zones(confFiles):
 			if zone_name.startswith('zone '):
 				zone_name = zone_name[6:-1].lower()
 				if zone_obj.get('file', None) and zone_obj.get('type', None):
+					zone = None
 					zone_changed = bool()
 					# check if zone name is in redis for user if so add it to real_zones instead, if not do as normal
 					changed = predis.get('username:'+zone_name)
 					if changed:
 						zone = changed
 						zone_changed = True
+						unsaved = True
 					else:
 						try:
 							zone = dns.zone.from_file(zone_obj['file'][1:-1], zone_name)
@@ -38,10 +40,30 @@ def get_local_zones(confFiles):
 							pass
 					if zone:
 						real_zones.append([zone_name, zone, zone_obj['type'], zone_changed])
-	slaves = [[z, real_zones.pop(real_zones.index(z))][0:2] for z in real_zones if z[2].lower() == 'slave']
-	reverses = [[z, real_zones.pop(real_zones.index(z))][0:2] for z in real_zones if z[0].lower().endswith('in-addr.arpa')]
-	domains = [z for z in real_zones[:]] # just the rest...?
+	slaves = []
+	reverses = []
+	domains = []
+	for z in real_zones:
+		if z[2].lower() == 'slave':
+			slaves.append(z)
+		elif z[0].lower().endswith('in-addr.arpa'):
+			reverses.append(z)
+		else:
+			domains.append(z)
 	return domains, reverses, slaves, unsaved
+
+def unrelativize(zone_name, name):
+	# make this a toggle option?
+	if str(name) == "@":
+		return zone_name+'.'
+	else:
+		return str(name)+'.'+zone_name+'.'
+
+def zone_file_diff(before_path, after):
+	before = None
+	file_name = None
+	difflib.unified_diff(before, after, fromfile=file_name, tofile=file_name)
+
 
 ###############
 # Redis Utils #
@@ -67,7 +89,34 @@ predis = PickledRedis()
 def index():
 	domains, reverses, slaves, unsaved = get_local_zones(config.bind_zone_confs)
 	notifications = [] # from logs?
-	return render_template('index.html', domains=domains, slaves=slaves, reverses=reverses, unsaved=unsaved, notifications=notifications)
+	return render_template('dashboard.html', domains=domains, slaves=slaves, reverses=reverses, unsaved=unsaved, notifications=notifications)
+
+@app.route('/domain/<string:domain_name>')
+@app.route('/slave/<string:slave_name>')
+@app.route('/reverse/<string:reverse_name>')
+def get_records(domain_name=None, slave_name=None, reverse_name=None):
+	domains, reverses, slaves, unsaved = get_local_zones(config.bind_zone_confs)
+	notifications = [] # from logs?
+	inspect_thing = None
+	if domain_name:
+		for d in domains:
+			if d[0] == domain_name:
+				inspect_thing = d
+	elif slave_name:
+		for s in slaves:
+			if s[0] == slave_name:
+				inspect_thing = s
+	elif reverse_name:
+		for r in reverses:
+			if r[0] == reverse_name:
+				inspect_thing = r
+	else:
+		return ""
+
+	if inspect_thing:
+		return render_template('zone.html', inspect_zone=inspect_thing, domains=domains, slaves=slaves, reverses=reverses, unsaved=unsaved, notifications=notifications, rtype_to_text=dns.rdatatype.to_text, unrelativize=unrelativize, str=str, len=len)
+	else:
+		return ""
 
 ##############
 # API routes #
