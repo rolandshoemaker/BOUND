04777d861b2dd7ff4fee7f462ff8ce2a220ab319
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-11-09 16:18:49 -0800

    auth stuff, yeah

--- bound.py
+++ bound.py
@@ -1,14 +1,24 @@
 import config
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, session, jsonify, abort, redirect, url_for, g
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from passlib.apps import custom_app_context as pwd_context
+
 import sys
 sys.path.insert(1, 'lib/')
 sys.path.insert(1, 'lib/iscpy/') # guh such ghetto-hack to make this work with python 3...
-import pickle, iscpy, difflib, subprocess
+import pickle, iscpy, difflib, subprocess, uuid
 import dns.zone, dns.rdatatype
 from redis import StrictRedis
 
+###############
+# Flask setup #
+###############
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(uuid.uuid4())
 
 #############
 # DNS Utils #
@@ -29,7 +39,7 @@ def get_local_zones(confFiles):
 					zone = None
 					zone_changed = bool()
 					# check if zone name is in redis for user if so add it to real_zones instead, if not do as normal
-					changed = predis.get('username:'+zone_name)
+					changed = predis.get(g.user.name+zone_name)
 					if changed:
 						zone = changed
 						zone_changed = True
@@ -103,11 +113,77 @@ class PickledRedis(StrictRedis):
 
 predis = PickledRedis()
 
+##############
+# Auth stuff #
+##############
+
+app.config['SQLALCHEMY_DATABASE_URI'] = config.user_token_store
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, name):
+    	self.name = name
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def is_authenticated(self):
+    	return True
+
+    def is_actuve(self):
+    	return True
+
+    def is_anonymous(self):
+    	return False
+
+    def get_id(self):
+    	return str(self.id) # python 3 unicoding yo
+
+    def __repr__(self):
+    	return '<User %r>' % (self.name)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(int(userid))
+
+@app.route('/login', methods=['POST', 'GET'])
+def login_view():
+	if g.user is not None and g.user.is_authenticated():
+		return redirect(url_for('index'))
+	if request.method == 'GET':
+		return render_template('login.html')
+	elif request.method == 'POST':
+		if request.args.username and request.args.password:
+			user = User.query.filter_by(name=request.args.username)
+			if user and user.verify_password(request.args.password):
+				remember_me = False
+				if 'remember_me' in request.args:
+					remember_me = request.args.remember_me
+				login_user(user, remember=remember_me)
+				return redirect(request.args.get('next') or url_for('index'))
+			else:
+				abort(400)
+		else:
+			abort(400)
+
+
 ####################
 # Front end routes #
 ####################
 
 @app.route('/')
+@login_required
 def index():
 	domains, reverses, slaves, unsaved = get_local_zones(config.bind_zone_confs)
 	notifications = [] # from logs?
@@ -116,6 +192,7 @@ def index():
 @app.route('/domain/<string:domain_name>')
 @app.route('/slave/<string:slave_name>')
 @app.route('/reverse/<string:reverse_name>')
+@login_required
 def get_records(domain_name=None, slave_name=None, reverse_name=None):
 	domains, reverses, slaves, unsaved = get_local_zones(config.bind_zone_confs)
 	notifications = [] # from logs?
@@ -144,12 +221,13 @@ def get_records(domain_name=None, slave_name=None, reverse_name=None):
 # API routes #
 ##############
 
-#replace this later with whatever api tool i end up using
 @app.route('/api/v1/bind_stats')
+@login_required
 def stats_endpoint():
 	return jsonify(get_bind_stats())
 
 @app.route('/api/v1/bind_running')
+@login_required
 def running_endpoint():
 	return jsonify({'running': get_bind_stats()['running']})
 
@@ -158,5 +236,12 @@ def running_endpoint():
 #############
 
 if __name__ == '__main__':
+	# Build DB and add admin user
+	db.create_all()
+	db.session.commit()
+	admin = User(name='admin')
+	admin.hash_password(config.default_admin_password)
+	db.session.add(admin)
+	db.session.commit()
 	app.debug = True
-	app.run(host=config.host, port=80)
+	app.run(host=config.host, port=config.port)
